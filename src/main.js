a178ab29dbed12cf3a5a175f4895d20936a21889
Kamen <sh.ibragim@gmail.com>
2022-03-05 22:54:48 +0300

    Add CalC(a,b,c)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-alert("AAAA");
+function calc(a,b,c){
+    if (a=="sum"){
+        console.log(b+c); 
+    }else if(a=="razn"){
+        console.log(b-c);
+    }else if (a=="del"){
+        if (c=0){
+        console.log("На ноль делить нельзя");    
+        }else {
+        console.log(b/c);
+        }
+    }else if(a=="multi"){
+        console.log(b*c);
+    }else if(a=="exp"){
+        if(c<1){
+            console.log("Степень < 1");
+        }else {
+            console.log(b**c);
+        }
+    }
+}
+
+calc('multi',2,4);
+

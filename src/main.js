e64a54e18d957027f742893b4b171f8b279f5bbd
Kamen <sh.ibragim@gmail.com>
2022-03-08 14:53:53 +0300

    2.0

--- src/main.js
+++ src/main.js
@@ -1,32 +1,25 @@
-function calc(a,b,c){
-    if (typeof a=="number"||typeof a=="boolean"||typeof a=="boolean"|| typeof b=="string"|| 
-    typeof c=="string"||typeof b=="boolean"||typeof c=="boolean") {
-        console.log('Не верные значания'); 
-        }
-        else {
-        if (a=="sum"){
-            console.log(b+c); 
-        }else if(a=="razn"){
-            console.log(b-c);
-        }else if (a=="del"){
-            if (c==0){
-            console.log('На ноль делить нельзя');    
-            }else {
-            console.log(b/c);
+function calc(a, b, c) {
+    if (typeof a == "string" || typeof b == "number" || typeof c == "number") {
+        if (a == "sum") {
+            console.log(b + c);
+        } else if (a == "razn") {
+            console.log(b - c);
+        } else if (a == "del") {
+            if (c == 0) {
+                console.log('На ноль делить нельзя');
+            } else {
+                console.log(b / c);
             }
-        }else if(a=="multi"){
-            console.log(b*c);
-        }else if(a=="exp"){
-            if(c<1){
+        } else if (a == "multi") {
+            console.log(b * c);
+        } else if (a == "exp") {
+            if (c < 1) {
                 console.log("Степень < 1");
-            }else {
-                console.log(b**c);
+            } else {
+                console.log(b ** c);
             }
-        }
-        else {
-            console.log("unknown operation");
-        }
-    
-    }
+        }else console.log('Не верные значания');
+    } 
 }
-calc(true,2,3);
+
+calc("arue", 2, 3);

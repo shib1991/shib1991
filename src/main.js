9f65fefb7483925987f8b74dc5499ea37543c8cd
Kamen <sh.ibragim@gmail.com>
2022-03-08 14:38:38 +0300

    Add definition typeof (a,b,c)

--- src/main.js
+++ src/main.js
@@ -1,27 +1,32 @@
 function calc(a,b,c){
-    if (a=="sum"){
-        console.log(b+c); 
-    }else if(a=="razn"){
-        console.log(b-c);
-    }else if (a=="del"){
-        if (c==0){
-        console.log('На ноль делить нельзя');    
-        }else {
-        console.log(b/c);
+    if (typeof a=="number"||typeof a=="boolean"||typeof a=="boolean"|| typeof b=="string"|| 
+    typeof c=="string"||typeof b=="boolean"||typeof c=="boolean") {
+        console.log('Не верные значания'); 
         }
-    }else if(a=="multi"){
-        console.log(b*c);
-    }else if(a=="exp"){
-        if(c<1){
-            console.log("Степень < 1");
-        }else {
-            console.log(b**c);
+        else {
+        if (a=="sum"){
+            console.log(b+c); 
+        }else if(a=="razn"){
+            console.log(b-c);
+        }else if (a=="del"){
+            if (c==0){
+            console.log('На ноль делить нельзя');    
+            }else {
+            console.log(b/c);
+            }
+        }else if(a=="multi"){
+            console.log(b*c);
+        }else if(a=="exp"){
+            if(c<1){
+                console.log("Степень < 1");
+            }else {
+                console.log(b**c);
+            }
         }
-    }
-    else {
-        console.log("unknown operation");
+        else {
+            console.log("unknown operation");
+        }
+    
     }
 }
-
-calc('del',2,0);
-
+calc(true,2,3);
